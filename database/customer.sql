9e3e087995cf3d7051b58f6818a520dbbbfd80c0
Nabiwin <nabinsaud2059@gmail.com>
2023-05-21 21:35:00 +0545

    feat: add single linked list

--- database/customer.sql
+++ database/customer.sql
@@ -83,3 +83,78 @@ FROM customer
     LEFT JOIN orders ON customer.customer_id = orders.customer_id
 WHERE customer.customer_id = 1
 ORDER BY orders.order_date;
+
+-- group by cases
+
+-- use aggregate error does not work order by in this cases
+
+SELECT
+    customer.customer_address,
+    customer.customer_id,
+    customer.customer_email,
+    MAX(orders.order_id) as orderID,
+    MIN(orders.order_date) as orderDate
+FROM customer
+    LEFT JOIN orders ON customer.customer_id = orders.customer_id
+GROUP BY customer.customer_id;
+
+--  does not work group by should contains non aggregate column
+
+SELECT
+    customer.customer_address,
+    MAX(customer.customer_id),
+    customer.customer_email,
+    orders.order_id as orderID,
+    orders.order_date as orderDate
+FROM customer
+    LEFT JOIN orders ON customer.customer_id = orders.customer_id
+GROUP BY customer.customer_id;
+
+--  the above need to be fixed like this
+
+-- this works while applying orders :)
+
+SELECT
+    customer.customer_address,
+    MAX(customer.customer_id) as customerId,
+    orders.order_id as orderID,
+    orders.order_date as orderDate
+FROM customer
+    LEFT JOIN orders ON customer.customer_id = orders.customer_id
+GROUP BY
+    customer.customer_id,
+    customer.customer_email,
+    orders.order_date,
+    orders.order_id;
+
+-- right join accessing customers from orders reverse way not gonna use too much in real stuff :)
+
+SELECT
+    orders.order_id,
+    orders.order_date,
+    customer.customer_address,
+    customer.customer_email,
+    customer.customer_id
+FROM orders
+    RIGHT JOIN customer ON orders.customer_id = customer.customer_id;
+
+-- inner join
+
+SELECT
+    customer.customer_id,
+    customer.customer_address,
+    customer.customer_email,
+    orders.order_date,
+    orders.order_id
+FROM customer
+    INNER JOIN orders ON customer.customer_id = orders.customer_id;
+
+-- full join or full outer join
+
+SELECT *
+FROM customer c
+    LEFT JOIN orders o ON c.customer_id = o.customer_id
+UNION
+SELECT *
+FROM customer c
+    RIGHT JOIN orders ON c.customer_id = orders.order_id;
--- linkedList/index.ts
+++ linkedList/index.ts
@@ -1,6 +1,51 @@
-class ListNode {
-  constructor(public data: number, public next: ListNode | null = null) {}
+class LinkedNode<T> {
+  value: T;
+  next: LinkedNode<T> | null;
+  constructor(value: T) {
+    this.value = value;
+    this.next = null;
+  }
 }
-class LinkedList {
-  constructor(public head: ListNode | null = null) {}
+
+class LinkedList<T> {
+  head: LinkedNode<T> | null;
+  tail: LinkedNode<T> | null;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value: T) {
+    const newNode = new LinkedNode(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      if (this.tail == null) {
+        throw new Error("tail should not be null");
+      }
+      //* If the linked list is not empty, update the next property of the current tail node to point to the new node.
+      this.tail.next = newNode;
+      this.tail = newNode; // update the references
+    }
+  }
+
+  print(): void {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      console.log("current node value is", currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
 }
+
+const linkedList = new LinkedList<number>();
+linkedList.append(10);
+linkedList.append(20);
+linkedList.append(30);
+linkedList.append(40);
+linkedList.append(50);
+linkedList.append(60);
+linkedList.append(70);
+// console.log("linked list is", linkedList);
+linkedList.print();
